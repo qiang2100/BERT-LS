e0a93a2196c47b5e18e7436d14a48456c7afaabb
Jipeng Qiang <qjp2100@gmail.com>
2020-06-25 16:31:13 +0800

    Update run_LSBert1.sh

--- run_LSBert1.sh
+++ run_LSBert1.sh
@@ -6,7 +6,7 @@ python3 LSBert1.py \
   --do_eval \
   --do_lower_case \
   --num_selections 10 \
-  --eval_dir $BERT_DIR/datasets/BenchLSd.txt \
+  --eval_dir $BERT_DIR/datasets/BenchLS.txt \
   --bert_model bert-large-uncased-whole-word-masking \
   --max_seq_length 250 \
   --word_embeddings /media/qiang/ee63f41d-4004-44fe-bcfd-522df9f2eee8/wikipedia/fastText/crawl-300d-2M-subword.vec\
